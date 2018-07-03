d3d38c3a1f8947acb0e822a6589974d17e5d20a0
Aritz <aritzmaeztu@hotmail.com>
2018-07-03 21:22:44 +0200

    Fixed integration test. Updated to JSF 2.3

--- src/test/java/org/xtremebiker/jsfspring/test/IntegrationTest.java
+++ src/test/java/org/xtremebiker/jsfspring/test/IntegrationTest.java
@@ -1,18 +1,23 @@
-package com.tesicnor.tcheck.ui.test.integration;
+package org.xtremebiker.jsfspring.test;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.reactive.server.WebTestClient;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 public class IntegrationTest {
 
+	@Autowired
+	private WebTestClient webClient;
+
 	@Test
 	public void test() {
-
+		this.webClient.get().uri("/ui/hello.xhtml").exchange().expectStatus().isOk();
 	}
 
 }
